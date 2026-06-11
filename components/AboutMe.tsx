8d92dfe51daf577365bcf49f1dc1f767ade4563f
Harshalvk <harshalvkhobragade@gmail.com>
2026-06-11 16:41:43 +0530

    fix: update about-me page

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,54 +1,64 @@
-import React from "react";
-import { Button } from "./ui/button";
-import { ArrowRight } from "lucide-react";
-import { SiGithub } from "react-icons/si";
-import Title from "./Title";
-import Link from "next/link";
+import React from 'react';
+import { Button } from './ui/button';
+import { ArrowRight } from 'lucide-react';
+import { SiGithub } from 'react-icons/si';
+import Link from 'next/link';
+import SectionBorders from './shared/SectionBorders';
+import { inter } from '@/lib/fonts';
+import { Title } from './Typography';
 
 const AboutMe = () => {
   return (
-    <section>
-      <Title text="About Me." />
-      <p className="flex flex-col gap-2 md:text-lg dark:text-muted-foreground font-mono leading-6">
-        <span>
-          I&apos;ve been coding for the past 3 years, mainly focusing on web
-          development and building scalable backend systems.
-        </span>
-        <span>
-          In this time, I&apos;ve worked with various technologies to create
-          fast, responsive applications, from backend architecture to frontend
-          development and deployment.
-        </span>
-        <span>
-          I enjoy solving real-world problems with code and constantly aim to
-          improve while staying current with tech trends.
-        </span>
-      </p>
-      <div className="my-5 flex items-center gap-5 flex-wrap">
-        <Button
-          variant={"secondary"}
-          className="flex items-center justify-center group"
+    <section className="border-border relative border border-t-0 border-b-0">
+      <SectionBorders />
+      <div className="bg-hatching screen-line-top screen-line-bottom h-4 w-full" />
+      <Title text="About Me." className="px-4 py-1" />
+      <div className="bg-hatching screen-line-top screen-line-bottom h-4 w-full" />
+
+      <div className="space-y-4 p-4">
+        <ul
+          className={`list-disc space-y-2 pl-6 text-lg text-zinc-600 dark:text-zinc-400 ${inter.style.fontFamily} marker:text-zinc-600 dark:marker:text-zinc-400`}
         >
-          <Link
-            href={"https://github.com/harshalvk/"}
-            target="_blank"
-            className="flex items-center gap-1"
+          <li>
+            I&apos;ve been coding for the past 3 years, mainly focusing on web development and
+            building scalable backend systems.
+          </li>
+          <li>
+            In this time, I&apos;ve worked with various technologies to create fast, responsive
+            applications, from backend architecture to frontend development and deployment.
+          </li>
+          <li>
+            I enjoy solving real-world problems with code and constantly aim to improve while
+            staying current with tech trends.
+          </li>
+        </ul>
+
+        <div className="flex flex-wrap items-center gap-5">
+          <Button
+            variant={'secondary'}
+            className="group flex items-center justify-center rounded-sm border"
           >
-            <SiGithub size={5} />
-            View my GitHub
+            <Link
+              href={'https://github.com/harshalvk/'}
+              target="_blank"
+              className="flex items-center gap-1"
+            >
+              <SiGithub size={5} />
+              View my GitHub
+              <ArrowRight
+                size={5}
+                className="-translate-x-0 transition duration-200 group-hover:translate-x-0.5"
+              />
+            </Link>
+          </Button>
+          <Button variant={'outline'} className="group rounded-sm">
+            Contact me
             <ArrowRight
               size={5}
-              className="-translate-x-0 group-hover:translate-x-0.5 transition duration-200"
+              className="-translate-x-0 transition duration-200 group-hover:translate-x-0.5"
             />
-          </Link>
-        </Button>
-        <Button variant={"outline"} className="group">
-          Contact me
-          <ArrowRight
-            size={5}
-            className="-translate-x-0 group-hover:translate-x-0.5 transition duration-200"
-          />
-        </Button>
+          </Button>
+        </div>
       </div>
     </section>
   );
